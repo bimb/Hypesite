2e6ef400a0afb5afb7a49c25e5ab62628281ab07
karkaim <kar@frutabomba.com.mx>
2017-01-05 17:03:32 -0600

    ajustes single

--- single.php
+++ single.php
@@ -13,20 +13,17 @@
 
 	<header>
 	    <h1 class="postTitle"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-	    <?php echo get_avatar( $comment, 32 ); ?>  
-	    <small><?php the_date(); ?> by <?php the_author(); ?></small>
+	     
 	</header>
 
       <section class="post"><?php the_content(__('(more...)')); ?></section>
-      <footer class="postMeta">Category: <?php the_category(', ') . " " . the_tags(__('Tags: '), ', ', ' | ') . edit_post_link(__('Edit'), ''); ?></footer>
-
+      
       <hr class="noCss" />
 
 	</article>
 
 	<?php
 
-  comments_template();
 
   endwhile; else: ?>
 
