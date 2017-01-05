c1789a17d4d9994aa1239f652a3bb9d8ac5213ef
karkaim <kar@frutabomba.com.mx>
2017-01-05 17:16:02 -0600

    single instructores

---
+++ single-instructores.php
@@ -0,0 +1,34 @@
+<?php
+
+  /**
+  *@desc A single blog post See page.php is for a page layout.
+  */
+
+  get_header();
+
+  if (have_posts()) : while (have_posts()) : the_post();
+  ?>
+
+	<article class="postWrapper" id="post-<?php the_ID(); ?>">
+
+	<header>
+	    <h1 class="postTitle"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+	</header>
+
+      <section class="post"><?php the_content(__('(more...)')); ?></section>
+
+	</article>
+
+	<?php
+
+
+  endwhile; else: ?>
+
+		<p>Sorry, no posts matched your criteria.</p>
+
+<?php
+  endif;
+
+  get_footer();
+
+?>
--- single.php
+++ single.php
@@ -1,37 +1,10 @@
 <?php
+$post = $wp_query->post;
 
-  /**
-  *@desc A single blog post See page.php is for a page layout.
-  */
-
-  get_header();
-
-  if (have_posts()) : while (have_posts()) : the_post();
-  ?>
-
-	<article class="postWrapper" id="post-<?php the_ID(); ?>">
-
-	<header>
-	    <h1 class="postTitle"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-	     
-	</header>
-
-      <section class="post"><?php the_content(__('(more...)')); ?></section>
-      
-      <hr class="noCss" />
-
-	</article>
-
-	<?php
-
-
-  endwhile; else: ?>
-
-		<p>Sorry, no posts matched your criteria.</p>
-
-<?php
-  endif;
-
-  get_footer();
-
+if ( in_category('instructores') ){
+    include(TEMPLATEPATH.'/single-instructores.php');
+}else{
+    include(TEMPLATEPATH.'/single-default.php');
+}
+   
 ?>
