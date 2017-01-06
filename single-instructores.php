4797bf55c5e18cf6aa583feb57de622fdee90bcf
karkaim <kar@frutabomba.com.mx>
2017-01-05 18:53:05 -0600

    instructores cat

--- single-instructores.php
+++ single-instructores.php
@@ -12,6 +12,7 @@
 	<article class="postWrapper" id="post-<?php the_ID(); ?>">
 
 	<header>
+      <?php if ( has_post_thumbnail() )the_post_thumbnail(); ?>
 	    <h1 class="postTitle"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 	</header>
 
