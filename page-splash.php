bde6d18a5165f29b56b3dbdc5654e5cce73ae0c0
karkaim <kar_nin@yahoo.com.mx>
2017-01-18 15:26:21 -0600

    splash ajustes

--- page-splash.php
+++ page-splash.php
@@ -1,7 +1,6 @@
-
 <html>
 <body style="background: #fff;">
-<img src="images/HYPE_Imagen-de-entrada.jpg" style="height: auto; width: 100%; display: block; margin: 0 auto; float: none;">
+<img src="http://hypetraining.mx/images/HYPE_Imagen-de-entrada.jpg" style="height: auto; width: 100%; display: block; margin: 0 auto; float: none;">
 
 </body>
 </html>
